1372bf8afb0eb977e7a8d747bc55e79be8724849
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-03-26 15:12:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const arr = ["Charlie", "Samip", "Ali"];
+function writeCards(arr, message) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr[i] = `Thank you, ${arr[i]}, for the wonderful ${message} gift!`;
+  }
+  return newArr;
+}
+console.log(writeCards(arr, "birthday"));
+
+const n = 10;
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+countDown(10);
